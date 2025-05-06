828ceaf3bbb366ea894bf5351060148445ad2cb2
Filip Bengtegård <1380257+Insanityandme@users.noreply.github.com>
2025-05-06 17:29:56 +0200

    feat: add basic html structure of main component

---
+++ src/components/UrlShortener.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import UrlCreator from './UrlCreator.vue'
+
+const url = ref('')
+
+function handleGeneratedUrl(payload) {
+  url.value = payload
+}
+</script>
+
+<template>
+  <section class="section-url-shortener">
+    <div class="container">
+      <UrlCreator @generatedUrl="handleGeneratedUrl" />
+    </div>
+  </section>
+  <section class="section-url-history">
+    <div class="container">
+      <!-- JOHANNES: Add component in here! -->
+    </div>
+  </section>
+
+</template>
+
