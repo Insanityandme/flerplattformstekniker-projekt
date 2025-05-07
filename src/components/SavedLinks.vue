ee7f77d2dc02658b5ef5cf919cd4bf7f6acbab1a
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2025-05-07 11:50:30 +0200

    feat: add saved links component

---
+++ src/components/SavedLinks.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+type Link = {
+  link: string
+  shortLink: string
+}
+
+const props = defineProps<{
+  links: Link[]
+}>()
+</script>
+
+<template>
+  <div class="container">
+    <h3>My shortened links</h3>
+    <ul>
+      <li v-for="(link, index) in props.links" :key="index">
+        <a :href="link.shortLink">{{ link.shortLink }} ({{ link.link }})</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
