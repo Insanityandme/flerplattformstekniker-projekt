27f97df9220441e59d3c09f9291c55a6c7fe8d9c
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2025-05-20 22:54:44 +0200

    feat: create link history card component

---
+++ src/components/LinkHistoryCard.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import type { urlData } from '@/types/UrlTypes'
+
+const copyToClipboard = (text: string) => {
+  navigator.clipboard.writeText(text)
+}
+
+const props = defineProps<{
+  id: number
+  linkData: urlData
+}>()
+</script>
+
+<template>
+  <div class="card-root">
+    <div class="card-top">
+      <button v-on:mousedown="copyToClipboard(props.linkData.shortLink)">
+        {{ props.linkData.shortLink }}
+      </button>
+      <img src="../assets/QRCode.svg" />
+    </div>
+    <div class="card-bottom">
+      <a :href="props.linkData.longLink" target="_blank">{{ props.linkData.longLink }}</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-root {
+  display: flex;
+  flex-direction: column;
+}
+
+.card-top {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-items: center;
+}
+.card-top > img {
+  width: 100%;
+  flex-basis: 20%;
+}
+.card-top > button {
+  flex-basis: 80%;
+  background-color: lightgreen;
+  border-radius: 16px;
+  padding: 0.5rem;
+  margin: 0.2rem;
+
+  text-wrap-mode: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.card-bottom {
+  display: flex;
+  justify-content: center;
+  padding: 0.2rem;
+}
+.card-bottom > a {
+  text-wrap-mode: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+}
+</style>
